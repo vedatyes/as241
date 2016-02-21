496df0923198d8cd967f7cd832315ec28be1f6d0
vedatyes <vedatyes@gmail.com>
2016-02-21 01:35:26 +0100

    Update wv6.js

--- wv6.js
+++ wv6.js
@@ -1,9 +1,9 @@
 
 var popunder=new Array()
 popunder[0]="http://bit.ly/1OmuQIF"
-popunder[1]="http://bit.ly/1OmuQIF"
+popunder[1]="http://bit.ly/1QrDMxF"
 popunder[2]="http://bit.ly/1OmuQIF"
-popunder[3]="http://bit.ly/1OmuQIF"
+popunder[3]="http://bit.ly/1QrDMxF"
 popunder[Math.floor(Math.random()*(popunder.length))]
 
 $(document).ready(function(){
